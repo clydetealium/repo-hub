88a93b92126fcd77c8353683c12369439fa4ae49
Clyde Tedrick <clyde.tedrick@tealium.com>
2023-12-28 13:02:12 -0500

    sort out form display

--- src/SchemaForm.js
+++ src/SchemaForm.js
@@ -1,91 +1,75 @@
-import React, { useState } from 'react';
-// import Form from '@rjsf/core';
+import React, { useState, useRef, useEffect } from 'react';
 import Form from '@rjsf/bootstrap-4';
 import validator from '@rjsf/validator-ajv8';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function SchemaForm({ schema, uiSchema }) {
   const [formData, setFormData] = useState({});
+  const leftPanelRef = useRef(null);
+  const rightPanelRef = useRef(null);
+  let prettyFormData = JSON.stringify(formData, null, 2);
 
   const onChange = ({ formData }) => {
     setFormData(formData);
-    // validator.validate(formData, schema);
+    prettyFormData = JSON.stringify(formData, null, 2);
   };
 
   const onSubmit = ({ formData }) => {
-    console.log('Form submitted:', formData);
+    const formState = JSON.stringify(formData, null, 2);
+
+    // Copy form state to clipboard
+    navigator.clipboard.writeText(formState)
+      .then(() => {
+        alert('Form state copied to clipboard!');
+      })
+      .catch((error) => {
+        console.error('Failed to copy form state:', error);
+      });
   };
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const firstChildHeight = entry.contentRect.height + 'px';
+        rightPanelRef.current.style.height = firstChildHeight;
+      }
+    });
+
+    if (leftPanelRef.current) {
+      observer.observe(leftPanelRef.current.firstElementChild);
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, [formData]);
 
   return (
     <div className="container">
-      <Form
-        schema={schema}
-        uiSchema={uiSchema}
-        validator={validator}
-        formData={formData}
-        onChange={onChange}
-        onSubmit={onSubmit}
-        onError={console.error}
-      />
+      <div className="row">
+        <div className="col-md-6" ref={leftPanelRef}>
+          <Form
+            schema={schema}
+            uiSchema={uiSchema}
+            validator={validator}
+            formData={formData}
+            onChange={onChange}
+            onSubmit={onSubmit}
+            onError={console.error}
+          />
+        </div>
+        <div className="col-md-6" ref={rightPanelRef}>
+          <textarea
+            className="form-control"
+            style={{ height: '100%' }}
+            rows={10}
+            value={prettyFormData}
+            readOnly
+          />
+        </div>
+      </div>
     </div>
   );
-
-  // return (
-  //   <div className="container">
-  //     <h1>Form Example</h1>
-  //     <Form
-  //       schema={schema}
-  //       validator={validate}
-  //       formData={formData}
-  //       onChange={onChange}
-  //       onSubmit={onSubmit}
-  //       onError={(errors) => console.log('Form errors:', errors)}
-  //       showErrorList={false}
-  //       liveValidate
-  //       ObjectFieldTemplate={({ properties }) => (
-  //         <div>
-  //           {properties.map((element, index) => (
-  //             <div className="mb-3" key={index}>
-  //               {element.content}
-  //             </div>
-  //           ))}
-  //         </div>
-  //       )}
-  //       ErrorList={({ errors }) => (
-  //         <ul className="list-unstyled">
-  //           {errors.map((error, index) => (
-  //             <li key={index} className="text-danger">
-  //               {error.stack}
-  //             </li>
-  //           ))}
-  //         </ul>
-  //       )}
-  //       ArrayFieldTemplate={({ items }) => (
-  //         <div>
-  //           {items.map((element, index) => (
-  //             <div key={index}>
-  //               {element.children}
-  //               <button
-  //                 type="button"
-  //                 className="btn btn-danger"
-  //                 onClick={element.onDropIndexClick(index)}
-  //               >
-  //                 Remove
-  //               </button>
-  //             </div>
-  //           ))}
-  //           <button
-  //             type="button"
-  //             className="btn btn-success"
-  //             onClick={items.onAddClick}
-  //           >
-  //             Add Item
-  //           </button>
-  //         </div>
-  //       )}
-  //     />
-  //   </div>
-  // );
 }
 
 export default SchemaForm;
